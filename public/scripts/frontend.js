1fe886267c3d960b7d335bac647c1e71a9d4773d
Xyphergrim <jc.xypher@gmail.com>
2017-03-29 21:37:48 +0000

    Updated frontend.js with another way of changing themes

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -1,7 +1,7 @@
 /* global $ localStorage*/
 
 $(document).ready(function(){
-    // Theme colors
+    //Theme colors
     var greenThemeBody = "#C9F5C9";
     var blueThemeBody = "#AEE3F5";
     var darkThemeBody = "#B0B2B5";
@@ -55,4 +55,39 @@ $(document).ready(function(){
         $("#about").css("background-color", localStorage.navTheme);
         $("#contact").css("background-color", localStorage.navTheme);
     }
+    
+    
+    
+    //=======================================================================================================
+    // DIFFERENT WAY BY LOADING IN DIFFERENT CSS FILES - both ways have lag issue when navigating through app
+    //=======================================================================================================
+    // if(!localStorage.newTheme){
+    //     var defaultTheme = "green";
+    //     localStorage.newTheme = defaultTheme;
+    // }
+    
+    // // When the page loads, look for the cookie value.
+    // var color = localStorage.newTheme;   
+    
+    // // If you find it
+    // if (color === "green") {
+    //     $('link[id="theme"]').attr('href', '/stylesheets/main.css');
+    // } else if (color === "blue") {
+    //     $('link[id="theme"]').attr('href', '/stylesheets/mainBlue.css');
+    // } else if (color === "dark") {
+    //     $('link[id="theme"]').attr('href', '/stylesheets/mainDark.css');
+    // }
+
+    // $("#theme-btn").on("click", function(){
+    //     if (color === "green") {
+    //         $('link[id="theme"]').attr('href', '/stylesheets/mainBlue.css');
+    //         localStorage.newTheme = "blue";
+    //     } else if (color === "blue") {
+    //         $('link[id="theme"]').attr('href', '/stylesheets/mainDark.css');
+    //         localStorage.newTheme = "dark"
+    //     } else if (color === "dark") {
+    //         $('link[id="theme"]').attr('href', '/stylesheets/main.css');
+    //         localStorage.newTheme = "green";
+    //     }
+    // });
 });
